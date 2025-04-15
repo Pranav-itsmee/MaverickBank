cfc7736dec0e58cd829dff21feb98ebedaa82539
Pranav <160521493+Pranav-itsmee@users.noreply.github.com>
2025-04-15 13:47:33 +0530

    Delete evaluation directory

--- evaluation/src/main/java/com/hex/evaluation/repository/MedicalHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hex.evaluation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hex.evaluation.model.MedicalHistory;
-
-public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Integer> {
-
-}
